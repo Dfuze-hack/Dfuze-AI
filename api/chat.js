394d53bd9c95bc54cec09e2d3f6fbfa08bac34ad
Dfuze-hack <devencrypted00@gmail.com>
2026-06-06 00:47:33 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -28,9 +28,9 @@ export default async function handler(req, res) {
       }),
     }
   );
+const data = await response.json();
 
-  const data = await response.json();
-  res.status(200).json({
-    reply: data.choices?.[0]?.message?.content || "No response",
-  });
+res.status(200).json({
+  reply: JSON.stringify(data)
+});
 }
